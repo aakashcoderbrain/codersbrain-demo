c06fd2265d42aa179720dbf6915aaa7a6a251884
aakashcoderbrain <aakash.choudhary@codersbrain.com>
2024-09-04 16:23:52 +0530

    [2]

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes, } from 'react-router-dom';
-import SignUp from './components/email-registration/SignUp';
-import Login from './components/email-login/Login';
-import Question from './components/quix/Question';
+// // import React from 'react';
+// // import { BrowserRouter, Route, Routes, } from 'react-router-dom';
+// import SignUp from './components/email-registration/SignUp';
+// import Login from './components/email-login/Login';
+// import Question from './components/quix/Question';
 
 
-function App() {
-  return (
-    <BrowserRouter>
-		<Routes>
-      <Route path="/" element={<SignUp/>} />
-      <Route path="/email_login" element={<Login/>} />
-      <Route path="/Question" element={<Question/>} />
 
-		</Routes>
-	</BrowserRouter>
-  );
-}
+// function App() {
+//   return (
+//     <BrowserRouter>
+// 		<Routes>
+//       <Route path="/" element={<SignUp/>} />
+//       <Route path="/email_login" element={<Login/>} />
+//       <Route path="/Question" element={<Question/>} />
 
-export default App;
+// 		</Routes>
+// 	</BrowserRouter>
+//   );
+// }
+
+// export default App;
